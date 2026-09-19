4eff4626d7df47c57a28a52a775bf82cfeb9afdf
jonnypas9112-blip <jonnypas9112@gmail.com>
2026-09-19 13:17:50 -0600

    Create precios-hino.js

---
+++ precios-hino.js
@@ -0,0 +1,146 @@
+window.HINO_PRECIOS = {
+  actualizado: "Septiembre 2026",
+  moneda: "MXN",
+  precios: {
+    "516 Semi Long": {
+      my: "2027",
+      lista: 1062000,
+      contado: 998000
+    },
+    "516 Long": {
+      my: "2027",
+      lista: 1083000,
+      contado: 1018000
+    },
+    "616 Semi Long": {
+      my: "2027",
+      lista: 1241000,
+      contado: 1135000
+    },
+    "616 Long": {
+      my: "2027",
+      lista: 1266000,
+      contado: 1156000
+    },
+    "718 Semi Long": {
+      my: "2027",
+      lista: 1303000,
+      contado: 1220000
+    },
+    "718 Long": {
+      my: "2027",
+      lista: 1328000,
+      contado: 1240000
+    },
+    "718 Super Long": {
+      my: "2027",
+      lista: 1344000,
+      contado: 1256000
+    },
+    "818 Semi Long": {
+      my: "2027",
+      lista: 1401000,
+      contado: 1309000
+    },
+    "818 Long": {
+      my: "2027",
+      lista: 1414000,
+      contado: 1321000
+    },
+    "818 Super Long": {
+      my: "2027",
+      lista: 1437000,
+      contado: 1342000
+    },
+    "616 HV Long": {
+      my: "2027",
+      lista: 1862000,
+      contado: 1790000
+    },
+    "816 HV Long": {
+      my: "2027",
+      lista: 2142000,
+      contado: 2060000
+    },
+    "816 HV Super Long": {
+      my: "2027",
+      lista: 2192320,
+      contado: 2108000
+    },
+    "FC 1024G": {
+      my: "2027",
+      lista: 1645000,
+      contado: 1576000
+    },
+    "FC 1024G AT": {
+      my: "2027",
+      lista: 1908000,
+      contado: 1828000
+    },
+    "FC 1024J": {
+      my: "2027",
+      lista: 1668000,
+      contado: 1598000
+    },
+    "FC 1024J AT": {
+      my: "2027",
+      lista: 1936000,
+      contado: 1855000
+    },
+    "FG 1628M": {
+      my: "2027",
+      lista: 1919000,
+      contado: 1919000
+    },
+    "FG 1628M AT": {
+      my: "2027",
+      lista: 2226000,
+      contado: 2226000
+    },
+    "FG 1628J": {
+      my: "2027",
+      lista: 1960000,
+      contado: 1960000
+    },
+    "FG 1628J Dump": {
+      my: "2027",
+      lista: 1960000,
+      contado: 1960000
+    },
+    "FG 1628P": {
+      my: "2027",
+      lista: 1942000,
+      contado: 1942000
+    },
+    "FG 1628P AT": {
+      my: "2027",
+      lista: 2253000,
+      contado: 2253000
+    },
+    "FG 1628S": {
+      my: "2027",
+      lista: 1965000,
+      contado: 1965000
+    },
+    "GH 1828P Leaf": {
+      my: "2027",
+      lista: 2063000,
+      contado: 2063000
+    },
+    "GH 1828S Leaf": {
+      my: "2027",
+      lista: 2082000,
+      contado: 2082000
+    },
+    "FL 2628T 6X2 Leaf": {
+      my: "2027",
+      lista: 2189000,
+      contado: 2189000
+    },
+    "FM 2628T 6X4 Leaf": {
+      my: "2027",
+      lista: 2312000,
+      contado: 2312000
+    }
+  }
+};
